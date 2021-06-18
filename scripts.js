4709a3f19ea91473145e52269bb7d0759431c93a
shantel.kemunto@student.moringaschool.com <kemuntoshantel0@gmail.com>
2021-06-18 22:21:07 +0300

    Add project names on the portfolio section

--- scripts.js
+++ scripts.js
@@ -1,17 +1,3 @@
-$(document).ready(function() {
-
-    $("#design").click(function(){
-      $("#design-showing").toggle();
-      $(".hide-design-icon").toggle();
-    });
-  
-    $("#development").click(function(){
-      $("#development-showing").toggle();
-      $(".hide-development-icon").toggle();
-    });
-  
-    $("#productManagement").click(function(){
-      $("#product-management-showing").toggle();
-      $(".hide-product-management-icon").toggle();
-    });
-});  
+$(".hover").hover(function(){
+    $(this).animate({opacity:'1'});
+})
